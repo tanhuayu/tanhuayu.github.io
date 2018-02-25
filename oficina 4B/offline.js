bd33fbba024a0cb51284d01b8bfa460942f46f12
tanhuayu <thy_yu@hotmail.com>
2018-02-25 16:23:13 -0300

    Add files via upload

---
+++ oficina 4B/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1519585847,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"media/essse.m4a",
+		"media/essse.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
